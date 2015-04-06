f6ce354493c0e4bf74f7ddcec141fcfa0f04fc3f
Bryan Ambriz <bambriz@ucsc.edu>
2015-04-05 17:54:46 -0700

    it has been done :D
    i guess it works

--- p1_example.py
+++ p1_example.py
@@ -4,25 +4,77 @@ from heapq import heappush, heappop
 
 
 def dijkstras_shortest_path(src, dst, graph, adj):
+  
+  prev = {}
+    dist = {}
     queue = []
-
-    dst[src] = 0
-    prev = {}
-    prev[src] = None
-
-    for v in graph:
-        if v != src:
-            dst[v] = float('inf')
-            prev[v] = None
-        queue.append(v)
-
+    dist[src] = 0
+    # explore starting at the source node
+    
+    #discovered[source] = True
+    heappush(queue, (dist[src],src))
+    
+    iterations = 0
+    
     while queue:
-        u = dst[src]
-        node_to_be_removed = src
-        for node in queue:
-            if dst[node] < u:
-                u = dst[node]
-                node_to_be_removed = node
+        
+       # iterations += 1
+        
+        u,u2 = heappop(queue)
+        
+        if u2 == dst: # early termination
+            break
+        neighbors = adj(graph,u2)
+        for v in neighbors:
+            dx = (abs(u2[0]-v[0]))
+            dy = (abs(u2[1]-v[1]))
+            rDis = sqrt(dx*dx + dy*dy)
+            alt = u + rDis
+            if v not in dist or alt < dist[v]:
+               # discovered[v] = True
+                #parent[v] = u
+                dist[v] = alt
+                prev[v] = u2
+                heappush(queue, (dist[v],v))
+    
+    if u2 == dst:
+        path = []
+        node = dst
+        while node in prev:
+            path.append(node)
+            node = prev[node]
+        path.append(src)
+        path.reverse()
+        return path
+    else:
+        return []
+
+
+
+
+
+
+  # queue = []
+
+   #  dist[src] = 0
+    # prev = {}
+    # prev[src] = None
+
+    # for v in graph:
+    #     if v != src:
+    #         dist[v] = float('inf')
+     #        prev[v] = None
+       #  heappush(v, queue)
+
+    # while queue:
+     #    u = dist[src]
+      # u =  heappop(u)
+     #  neighbors = adj(graph, u)
+      #   for v in neighbors:
+        #     #alt = dist[u] + 
+         #    if dist[node] < u:
+          #       u = dist[node]
+            #     node_to_be_removed = node
 
 
     #queue = [src]
